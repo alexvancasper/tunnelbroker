02f8b09b2605acae220ea45984b7741bea92c21b
Alexander Ildiyarov <alexvancasper@gmail.com>
2024-02-27 07:12:16 +0000

    one queue for everything

--- agent/cmd/main.go
+++ agent/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 	"sync"
+	"time"
 
 	"github.com/alexvancasper/TunnelBroker/agent/internal/broker"
 	"github.com/alexvancasper/TunnelBroker/agent/internal/doer"
@@ -36,21 +37,16 @@ func main() {
 
 	MyLogger.Debug("Message broker connected")
 
-	addMsgs, err := m.AddRegisterConsumer()
+	msgs, err := m.AddRegisterConsumer()
 	if err != nil {
 		MyLogger.Fatalf("AddRegisterConsumer error: %s", err)
 	}
-	delMsgs, err := m.DeleteRegisterConsumer()
-	if err != nil {
-		MyLogger.Fatalf("DeleteRegisterConsumer error: %s", err)
-	}
-
 	var wg sync.WaitGroup
 	ctx, ctxCancel := context.WithCancel(context.Background())
 
 	MyLogger.Info(" [*] Waiting for messages. To exit press CTRL+C")
 	wg.Add(1)
-	go Listener(ctx, addMsgs, delMsgs, MyLogger, &wg)
+	go Listener(ctx, &wg, msgs, MyLogger)
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
@@ -60,26 +56,28 @@ func main() {
 
 }
 
-func Listener(ctx context.Context, addMsgs, delMsgs <-chan amqp091.Delivery, log *logrus.Logger, wg *sync.WaitGroup) {
+func Listener(ctx context.Context, wg *sync.WaitGroup, msgs <-chan amqp091.Delivery, log *logrus.Logger) {
 	defer wg.Done()
 	l := log.WithField("function", "Listener")
 	h := doer.Handler{
 		Log: log,
 	}
 	for {
+		time.Sleep(100 * time.Millisecond)
 		select {
 		case <-ctx.Done():
 			l.Debug("Context closed")
 			return
-		case addmsg := <-addMsgs:
-			l.Debugf("Received from ADD queue a message: %s", addmsg.Body)
-			wg.Add(1)
-			go h.AddTunnel(wg, addmsg.Body)
-		case delmsg := <-delMsgs:
-			l.Debugf("Received from DELETE queue a message: %s", delmsg.Body)
-			wg.Add(1)
-			go h.DeleteTunnel(wg, delmsg.Body)
+		case msg := <-msgs:
+			l.WithField("message type", msg.Type).WithField("body", msg.Body).Infof("Received message from queue")
+			switch msg.Type {
+			case string(broker.ADD):
+				wg.Add(1)
+				go h.AddTunnel(wg, msg.Body)
+			case string(broker.DELETE):
+				wg.Add(1)
+				go h.DeleteTunnel(wg, msg.Body)
+			}
 		}
 	}
-
 }
