68c19bda1a7dc83d97c85c8e27cb3ffe42d7455e
Alexander Ildiyarov <alexvancasper@gmail.com>
2024-07-28 12:10:16 +0000

    update tunnel - need to check

--- internal/tunnels/ipv6Prefix_test.go
+++ internal/tunnels/ipv6Prefix_test.go
@@ -17,47 +17,58 @@ func TestGetEndpoints(t *testing.T) {
 		"pre-test-5": {
 			v6addr:   "2001:0db8:85a3:0000:0000:8a2e:0370:7334/127",
 			network:  "2001:db8:85a3::8a2e:370:7334/127",
-			nextAddr: "2001:db8:85a3::8a2e:370:7335/127"},
+			nextAddr: "2001:db8:85a3::8a2e:370:7335/127",
+		},
 		"pre-test-7": {
 			v6addr:   "2001:0db8:85a3:0000:0000:8a2e:0370:0001/127",
 			network:  "2001:db8:85a3::8a2e:370:0/127",
-			nextAddr: "2001:db8:85a3::8a2e:370:1/127"},
+			nextAddr: "2001:db8:85a3::8a2e:370:1/127",
+		},
 		"pre-test-8": {
 			v6addr:   "2001:0db8:85a3::fafd/64",
 			network:  "2001:db8:85a3::/64",
-			nextAddr: "2001:db8:85a3::1/64"},
+			nextAddr: "2001:db8:85a3::1/64",
+		},
 		"pre-test-9": {
 			v6addr:   "2001:db8:85a3::/87",
 			network:  "2001:db8:85a3::/87",
-			nextAddr: "2001:db8:85a3::1/87"},
+			nextAddr: "2001:db8:85a3::1/87",
+		},
 		"pre-test-10": {
 			v6addr:   "2F7F:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF/3",
 			network:  "2000::/3",
-			nextAddr: "2000::1/3"},
+			nextAddr: "2000::1/3",
+		},
 		"pre-test-1": {
 			v6addr:   "2001:0db8:85a3:0000:0000:8a2e:0370:7334/127",
 			network:  "2001:db8:85a3::8a2e:370:7334/127",
-			nextAddr: "2001:db8:85a3::8a2e:370:7335/127"},
+			nextAddr: "2001:db8:85a3::8a2e:370:7335/127",
+		},
 		"pre-test-2": {
 			v6addr:   "2001:0db8:85a3:0000:0000:8a2e:0370:FFFF/64",
 			network:  "2001:db8:85a3::/64",
-			nextAddr: "2001:db8:85a3::1/64"},
+			nextAddr: "2001:db8:85a3::1/64",
+		},
 		"pre-test-3": {
 			v6addr:   "2001:0db8:85a3::/87",
 			network:  "2001:db8:85a3::/87",
-			nextAddr: "2001:db8:85a3::1/87"},
+			nextAddr: "2001:db8:85a3::1/87",
+		},
 		"pre-test-4": {
 			v6addr:   "2001:0db8:85a3:FFFF:FFFF:FFFF:FFFF:FFFF/10",
 			network:  "2000::/10",
-			nextAddr: "2000::1/10"},
+			nextAddr: "2000::1/10",
+		},
 		"pre-test-11": {
 			v6addr:   "2FFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF/3",
 			network:  "2000::/3",
-			nextAddr: "2000::1/3"},
+			nextAddr: "2000::1/3",
+		},
 		"pre-test-12": {
 			v6addr:   "2FFF:FFFF:FFFF:FFFF:FFFF:FF00:FF00:FFFF/112",
 			network:  "2fff:ffff:ffff:ffff:ffff:ff00:ff00::/112",
-			nextAddr: "2fff:ffff:ffff:ffff:ffff:ff00:ff00:1/112"},
+			nextAddr: "2fff:ffff:ffff:ffff:ffff:ff00:ff00:1/112",
+		},
 	}
 
 	for name, testCase := range tests {
